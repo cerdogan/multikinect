0b59828d2d8df6e5b32100cecdc635bcb09148e8
Can Erdogan <canerdogan.89@gmail.com>
2015-11-28 21:27:34 -0500

    Find relative transformation by detecting tennis balls and creating a frame

--- all4.cpp
+++ all4.cpp
@@ -1,61 +1,72 @@
 #include <thread>
+#include <vector>
 #include <pcl/io/openni_grabber.h>
 #include <pcl/visualization/cloud_viewer.h>
+#include <pcl/filters/filter.h>
 
-using namespace std;
+typedef pcl::PointCloud<pcl::PointXYZRGBA> Cloud;
+typedef pcl::visualization::PCLVisualizer Viewer;
+pthread_mutex_t lock;
+std::vector <Cloud> cs;
 
-class SimpleOpenNIViewer
-{
-	public:
-		int id;
-		SimpleOpenNIViewer (int i_, char* buf) : viewer(buf) {
-			id = i_;
-		}
+/* ******************************************************************************************** */
+struct SimpleOpenNIViewer {
+	int id;
+	SimpleOpenNIViewer (int i_) : id(i_) { }
 
-		void cloud_cb_ (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud)
-		{
-			printf("%d\n", id);
-			fflush(stdout);
-//			if (!viewer.wasStopped())
-//				viewer.showCloud (cloud);
-		}
+	void cloud_cb_ (const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr &cloud) {
+		Cloud c2;
+		c2 += *cloud;
+		std::vector<int> indices;
+    pcl::removeNaNFromPointCloud(c2,c2,indices);
+    pthread_mutex_lock(&lock);
+		cs[id-1] = c2;
+    pthread_mutex_unlock(&lock);
+	}
 
-		void run ()
-		{
+	void init () {
 		char buf [256];
 		sprintf(buf, "#%d", id);
-			pcl::Grabber* interface = new pcl::OpenNIGrabber(buf);
-
-			boost::function<void (const pcl::PointCloud<pcl::PointXYZ>::ConstPtr&)> f =
-				boost::bind (&SimpleOpenNIViewer::cloud_cb_, this, _1);
-
-			interface->registerCallback (f);
-
-			interface->start ();
-
-			while(true) sleep(1);
-//			while (!viewer.wasStopped()) {
-//				boost::this_thread::sleep (boost::posix_time::seconds (1));
-//			}
-
-			interface->stop ();
-		}
-
-		pcl::visualization::CloudViewer viewer;
+		pcl::Grabber* interface = new pcl::OpenNIGrabber(buf);
+		boost::function<void (const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr&)> f =
+			boost::bind (&SimpleOpenNIViewer::cloud_cb_, this, _1);
+		interface->registerCallback (f);
+		interface->start ();
+	}
 };
 
-void threadFunc (int id) {
-	char buf [256];
-	sprintf(buf, "PCL OpenNI Viewer-%d", id);
-	SimpleOpenNIViewer v(id, buf);
-	v.run ();
-}
-
+/* ******************************************************************************************** */
 int main () {
 	
-	std::thread k1 (threadFunc, 1);
-	std::thread k2 (threadFunc, 2);
-	k1.join();
-	return 0;
+	for(int i = 0; i < 4; i++) cs.push_back(Cloud());
+	pthread_mutex_init(&lock, NULL);
+	SimpleOpenNIViewer v1(1), v2(2);
+	v1.init();
+	v2.init();
 
+	// Create the viewer
+	Viewer* viewer = new Viewer ("3D Viewer");
+  viewer->setBackgroundColor (0, 0, 0);
+  viewer->addCoordinateSystem (1.0);
+  viewer->initCameraParameters ();
+	viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE,3,"sample");
+
+	// Visualize
+	while(true) {
+		Cloud c;
+    pthread_mutex_lock(&lock);
+		for(int i = 0; i < 2; i++) {
+			printf("%d\t", cs[i].size());
+			c += (cs[i]);
+		}
+		printf("\n");
+    pthread_mutex_unlock(&lock);
+
+		viewer->removePointCloud();
+		Cloud::Ptr cp = c.makeShared();
+		pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGBA> rgb(cp);
+		viewer->addPointCloud(cp, rgb);
+		viewer->spinOnce();
+	//	printf("%d\n", c.size());
+	}
 }
